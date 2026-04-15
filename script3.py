0ffab229ee2a33a5cf4504c1f07248a9f2be9f25
Edwin <edwin.aramburo1@gmail.com>
2026-04-15 09:54:13 -0400

    training model

--- script3.py
+++ script3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 import numpy as np
 
 
@@ -12,6 +13,7 @@ def load_data(filename):
         return data
     except FileNotFoundError: 
         print(f"Error: File 'fake_file.csv' not found.")
+        retur None
 def validate_data(data):
     try:
         print(f"First 5 row!\n{data.head()}\n")
@@ -26,9 +28,24 @@ def validate_data(data):
 def train_model(data):
     try:
         print(f"Here is some data information\n{data.describe().astype(int)}\n")
+        X = data['Size (sqft)'].values.reshape(-1, 1)
+        y = data['Price ($)'].values
+        model = LinearRegression()
+        model.fit(X, y)
+
+        y_pred = model.predict(X)
+        r2 = r2_score(y, y_pred)
+
+        return model, X, y, r2
+
     except TypeError:
         print(f"{TypeError} has occurred\n")
-# def predict_price():
+def predict_price():
+    try: 
+        r2 = r2_score(y, y_pred)
+        print(f"The model predicted with an {r2} accuracy")
+    except TypeError:
+        print(f"{TypeError} has occurred\n")
 
 def main():
     data = load_data("housing_data.csv")
